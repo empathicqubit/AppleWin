2b7601d9b90601690f3978d6335c817327c0efe7
empathicqubit <empathicqubit@entan.gl>
2022-03-27 22:12:13 +0200

    GHA package build script

--- source/linux/build.sh
+++ source/linux/build.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+BASEDIR="$(dirname "$(readlink -e "${BASH_SOURCE}")")/../.."
 
 set -euxo pipefail
 
-mkdir -p build
+BUILD_QAPPLE=""
+if [ "${INSTALL_DEPS:-}" = "QT" ] ; then
+    BUILD_QAPPLE="-DBUILD_QAPPLE=1"
+    apt-get update
+    apt-get install -y --no-install-recommends qtbase5-dev qtmultimedia5-dev libqt5gamepad5-dev
+fi
+
+if [ ! -z "${INSTALL_DEPS:-}" ] ; then
+    apt-get update
+    apt-get install -y --no-install-recommends cmake make gcc g++ libyaml-dev libminizip-dev libboost-program-options-dev libncurses-dev libevdev-dev libsdl2-dev libsdl2-image-dev libgl-dev libpcap-dev
+    apt-get install -y --no-install-recommends libslirp-dev || echo "Skipping libslirp"
+fi
+
+cd "$BASEDIR"
+mkdir -p build/packages
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=RELEASE ..
-make -j 2
+cmake "$@" -DBUILD_APPLEN=1 $BUILD_QAPPLE -DBUILD_SA2=1 -DCMAKE_BUILD_TYPE=RELEASE "$BASEDIR"
+make -j $(nproc)
+cpack -G DEB -B "$BASEDIR/build/packages"
